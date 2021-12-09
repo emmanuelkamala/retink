adddde5418b0ac7c9547d5a835b99ef3d2f32064
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-12-09 16:30:52 +0300

    Create index.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Brand, Cta, Navbar } from './components';
+import { Contact, Footer, Header, Services } from './containers';
  
 function App() {
   return (
     <div className="App">
-      
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <About />
+      <Services />
+      <Cta />
+      <Contact />
+      <Footer />
     </div>
   );
 }
